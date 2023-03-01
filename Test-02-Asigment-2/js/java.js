643964f8fb6ff027ff8dce21751493c16360ff3d
Adis Deliomerovic <adis.deliomerovic@gmail.com>
2023-03-01 10:37:09 +0100

    test2 fix

--- Test-02-Asigment-2/js/java.js
+++ Test-02-Asigment-2/js/java.js
@@ -4,31 +4,63 @@ var usernameInput = document.getElementById('username');
 var passwordInput = document.getElementById('password');
 
 // Događaj "submit"
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (e) {
     // Spriječavanje podrazumijevanog ponašanja forme
-    event.preventDefault();
+    e.preventDefault();
+    
 
     // Provjera unesenih podataka
     var errors = [];
     if (usernameInput.value.trim() === '') {
         usernameInput.classList.add('invalid');
+        var errorDiv = document.createElement('div');
+        errorDiv.classList.add('error');
+        errorDiv.innerText = 'Please enter username.';
+        usernameInput.parentNode.insertBefore(errorDiv, usernameInput.nextSibling);
+        var icon = document.createElement('i');
+        icon.classList.add('fas', 'fa-times', 'error-icon');
+        usernameInput.parentNode.insertBefore(icon, usernameInput.nextSibling);
         errors.push('Please enter username.');
+    }
+    if (usernameInput.value.trim() !== 'new_user' && usernameInput.value.trim() !== '') {
+        usernameInput.classList.add('invalid');
+        var errorDiv = document.createElement('div');
+        errorDiv.classList.add('error');
+        errorDiv.innerText = 'Please enter valid username.';
+        usernameInput.parentNode.insertBefore(errorDiv, usernameInput.nextSibling);
+        var icon = document.createElement('i');
+        icon.classList.add('fas', 'fa-times', 'error-icon');
+        usernameInput.parentNode.insertBefore(icon, usernameInput.nextSibling);
+        errors.push('Please enter valid username.');
     }
     if (passwordInput.value.trim() === '') {
         passwordInput.classList.add('invalid');
+        var errorDiv = document.createElement('div');
+        errorDiv.classList.add('error');
+        errorDiv.innerText = 'Please enter password.';
+        passwordInput.parentNode.insertBefore(errorDiv, passwordInput.nextSibling);
+        var icon = document.createElement('i');
+        icon.classList.add('fas', 'fa-times', 'error-icon');
+        passwordInput.parentNode.insertBefore(icon, passwordInput.nextSibling);
         errors.push('Please enter password.');
     }
-    if (usernameInput.value.trim() !== 'new_user' || passwordInput.value.trim() !== '123456789') {
-        usernameInput.classList.add('invalid');
+    if (passwordInput.value.trim() !== '123456789' && passwordInput.value.trim() !== '') {
         passwordInput.classList.add('invalid');
-        errors.push('Invalid username or password.');
+        var errorDiv = document.createElement('div');
+        errorDiv.classList.add('error');
+        errorDiv.innerText = 'Please enter valid password.';
+        passwordInput.parentNode.insertBefore(errorDiv, passwordInput.nextSibling);
+        var icon = document.createElement('i');
+        icon.classList.add('fas', 'fa-times', 'error-icon');
+        passwordInput.parentNode.insertBefore(icon, passwordInput.nextSibling);
+        errors.push('Please enter valid password.');
     }
 
     // Ako ima grešaka, ispisati ih ispod forme
     var errorsDiv = document.createElement('div');
     errorsDiv.classList.add('errors');
     if (errors.length > 0) {
-        errors.forEach(function(error) {
+        errors.forEach(function (error) {
             var errorDiv = document.createElement('div');
             errorDiv.classList.add('error');
             errorDiv.innerText = error;
@@ -45,8 +77,53 @@ form.addEventListener('submit', function(event) {
     successDiv.classList.add('success');
     successDiv.innerText = 'Successful login!';
     form.appendChild(successDiv);
-    
+
     // Resetovanje polja
     usernameInput.value = '';
     passwordInput.value = '';
-});
+
+
+    if (errors.length > 0) {
+        // Ukloni sve prethodno postavljene ikone i poruke o greškama
+        var errorIcons = document.querySelectorAll('.error-icon');
+        var errorMessages = document.querySelectorAll('.error');
+        errorIcons.forEach(function (icon) {
+            icon.parentNode.removeChild(icon);
+        });
+        errorMessages.forEach(function (msg) {
+            msg.parentNode.removeChild(msg);
+        });
+        
+        usernameInput.classList.add('valid');
+        passwordInput.classList.add('valid');
+        var successDiv = document.createElement('div');
+        successDiv.classList.add('success');
+        successDiv.innerText = 'Successful login!';
+        form.parentNode.insertBefore(successDiv, form.nextSibling);
+        
+        usernameInput.value = '';
+        passwordInput.value = '';
+        
+        /*reset success  div*/
+        setTimeout(function() {
+            form.parentNode.removeChild(successDiv);
+        }, 3000);
+    } else {
+        errors.forEach(function (error) {
+            var errorDiv = document.createElement('div');
+            errorDiv.classList.add('error');
+            errorDiv.innerText = error.message;
+            var icon = document.createElement('i');
+            icon.classList.add('fas', 'fa-times', 'error-icon');
+            if (error.field === 'username') {
+                usernameInput.classList.add('invalid');
+                usernameInput.parentNode.insertBefore(errorDiv, usernameInput.nextSibling);
+                usernameInput.parentNode.insertBefore(icon, usernameInput.nextSibling);
+            } else {
+                passwordInput.classList.add('invalid');
+                passwordInput.parentNode.insertBefore(errorDiv, passwordInput.nextSibling);
+                passwordInput.parentNode.insertBefore(icon, passwordInput.nextSibling);
+            }
+        });
+    }
+});    
